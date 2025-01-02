3f210e683a3d545dc5a2ad1d34e81c7f2c44dd26
ChungaSaurusRex <polaris342@gmail.com>
2025-01-02 03:54:13 -0600

    Add files via upload

--- zandatsu/keymaps/default/keymap.c
+++ zandatsu/keymaps/default/keymap.c
@@ -1,6 +1,8 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later 
 // qmk compile -kb zandatsu -km default
+// cd qmk_firmware
+// qmk flash zandatsu_default.hex
 /*
      *
 	 -------------Physical Layout Approximation--------------------
@@ -40,6 +42,26 @@
      */
 
 #include QMK_KEYBOARD_H
+
+
+
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  debug_keyboard=true;
+  //debug_mouse=true;
+}
+
+#define DEBUG_MATRIX_SCAN_RATE
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // If console is enabled, it will print the matrix position and status of each key pressed
+  #ifdef CONSOLE_ENABLE
+    uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+  #endif 
+    return true;
+}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
