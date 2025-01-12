afdcbf80d0e6c377dd2f7d8e6262f56d25c64683
ChungaSaurusRex <polaris342@gmail.com>
2025-01-12 04:59:16 -0600

    Add files via upload

--- zandatsu/keymaps/default/keymap.c
+++ zandatsu/keymaps/default/keymap.c
@@ -1,51 +1,58 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later 
-// qmk compile -kb zandatsu -km default
+// Quick MSYS commands:
+// 	qmk compile -kb zandatsu -km default
+// 	cd qmk_firmware
+// 	qmk flash zandatsu_default.hex
+// 	qmk flash zandatsu_default.uf2 -e CONVERT_TO=rp2040_ce (with RP2040)
 /*
-     *
-	 -------------Physical Layout Approximation--------------------
-	     16 buttons, 26 keys in alphabet, /2 = 13 keys in 2 layers -- Labelled with column numbers.
-	 
-	          (0) (1) (2) 
-			  
-	          (0) (1) (2) (3)
-	    (0)   (1) (2) (3) (3)
-	    (0)   (1) (2) (3) 
-	 
-	 
-				          (3)
-					
-		TRIAL Simple Alphanumeric Typing Keymaps -- 1st half
-
-	[0] = LAYOUT(
-		KC_TAB,		KC_LSFT,	KC_ENT,		KC_G
-        KC_Q,   	KC_W,   	KC_E,   	KC_R,
-        KC_A,   	KC_S,   	KC_D,   	KC_F,
-        KC_Z,   	KC_X,   	KC_C,   	KC_V,
-											TG(1)
-		)
-
-							  
-		TRIAL Simple Alphanumeric Typing Keymaps -- 2nd half
-
-	[1] = LAYOUT(
-		KC_TAB,		KC_LSFT,	KC_ENT,		KC_P,
-        KC_T,   	KC_Y,   	KC_U,   	KC_I,
-        KC_H,   	KC_J,   	KC_K,   	KC_L,
-        KC_N,   	KC_M,   	KC_O,   	KC_B,
-											TG(0)
-		)
-					
-		
+   ...(0)(1)(2)(3)(4)
+(0)       X  X  X
+(1)       X  X  X  X
+(2)    X  X  X  X  X
+(3)    X  X  X  X  X
      */
 
 #include QMK_KEYBOARD_H
 
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  debug_keyboard=true;
+  //debug_mouse=true;
+}
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // If console is enabled, it will print the matrix position and status of each key pressed
+  #ifdef CONSOLE_ENABLE
+    uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+  #endif 
+    return true;
+}
+
+enum layers{
+	zandatsu,
+	rgb_config,
+	xbox_emu
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-				KC_A,   KC_E,   KC_I,
-				KC_B,   KC_F,   KC_J,   KC_N,
-		KC_C,   KC_G,   KC_K,   KC_O,	KC_Q,
-		KC_D,   KC_H,   KC_L,   KC_P,	KC_M
-	)                           
+    [xbox_emu] = LAYOUT(
+				 JS_0, JS_1, KC_ENT,
+				 KC_Q, KC_E, KC_R, KC_N,
+		KC_LSFT, KC_X, KC_F, KC_G, KC_M,
+		KC_LCTL, KC_Z, KC_C, KC_V, TO(zandatsu)
+	),		
+	[rgb_config] = LAYOUT(
+				 UG_TOGG, UG_NEXT, UG_HUEU,
+				 UG_VALU, KC_F, KC_J, UG_SATU,
+		KC_LSFT, KC_G, KC_K, KC_O, UG_SPDU,
+		KC_LCTL, KC_X, KC_C, KC_V, TO(xbox_emu)
+	),
+    [zandatsu] = LAYOUT(
+				 KC_LALT, KC_TAB, KC_ENT,
+				 KC_Q, KC_E, KC_R, KC_N,
+		KC_LSFT, KC_X, KC_F, KC_G, KC_M,
+		KC_LCTL, KC_Z, KC_C, KC_V, TG(rgb_config)
+	)	
 };
